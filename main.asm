05594cd6d4ede0fbf78f4d776f730227618e79a0
unknown <johanfredin@fastmail.com>
2023-05-06 20:45:56 +0200

    charsets

--- main.asm
+++ main.asm
@@ -3,51 +3,50 @@
 
  org $2000                                                  ; origin line, tells the assembler where our program will live in memory
 
-SAVMSC = $0058                                              ; Screen memory adress. The lowest adddress of the screen memory, corresponding to the upper left corner
-                                                            ; of the screen (where the value at this addresss will be displayed)
 SDLSTL = $0230                                              ; Starting address for our display list (dlist)
-
+CHBAS = $02f4                                               ; Character base register
+ 
+charset = $3c00                                             ; allocate space for character set, there are 4 pages and each is 256 bytes in size, 
+                                                            ; we will put it right before our screen memory
 screen = $4000                                              ; screen buffer
 blank8 = $70                                                ; 8 blank lines 
 lms = $40                                                   ; LMS = Load Memory Scan
 jvb = $41                                                   ; Jump While Vertical blank 
 
-antic2 = $2                                                 ; Antic mode 2
-antic3 = $3                                                 ; Antic mode 3
-antic4 = $4                                                 ; Antic mode 4
-antic5 = $5                                                 ; Antic mode 5
-antic6 = $6                                                 ; Antic mode 6
-antic7 = $7                                                 ; Antic mode 7
+antic2 = 2                                                 ; Antic mode 2
+antic5 = 5                                                 ; Antic mode 5
 
 
 ; Loads our display list
-    lda #<dlist                                             ; load in the first byte of our dlist into a reg
-    sta SDLSTL
-    lda #>dlist
-    sta SDLSTL+1
+    mwa #dlist SDLSTL                                      ; (mwa=move word) load in the first byte of our dlist into A reg and then the second
 
+; Set up character set    
+    mva #>charset CHBAS                                    ; (mva=move byte) move the high byte of charset into CHBAS address
 
-; Main loop
-    ldy #0                                                  ;# = the litteral value 
-loop
-    lda hello, y                                            ; will load the letter H into accumulator
-    sta screen, y
-    iny                                                     ; inc y
-    cpy #12                                                 ; compare value in y register with abolute value 12
-    bne loop                                                ; keep looping if y != 12
- 
-    jmp *                                                   ; endless loop
+    ldy #0
+print     
+    mva chars,y  charset,y 
+    iny
+    cpy #8
+    bne print
 
+    jmp *
 
 ; Display list
 dlist
     .byte blank8, blank8, blank8                             ; 24(8*3) blank lines
-    .byte antic5 + lms, <screen, >screen                     ; < = left shift, > = right shift
+    .byte antic2 + lms, <screen, >screen                     ; < = left shift, > = right shift
     .byte antic5, antic5, antic5, antic5, antic5, antic5
     .byte antic5, antic5, antic5, antic5, antic5
     .byte jvb <dlist, >dlist
 
-; Data
-hello
-    .byte "HELLO ATARI!"
+chars
+    .byte %00000001
+    .byte %00000010
+    .byte %00000100
+    .byte %00001000
+    .byte %00010000
+    .byte %00100000
+    .byte %01000000
+    .byte %10000000
  
